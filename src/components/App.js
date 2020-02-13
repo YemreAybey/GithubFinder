dc52f67578c353998ecbf45b4c4a8fdde652c765
Yemre <aybey77@gmail.com>
2020-02-13 18:03:59 +0300

    Creation of App

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = props => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className="fab fa-github"></i> Github Finder
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
